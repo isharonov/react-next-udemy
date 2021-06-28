cdffc998e623eecc4ee6979b6fa0c4742eca727b
Ivan Sharonov <48245524+isharonov@users.noreply.github.com>
2021-06-28 23:21:54 +0300

    15-81. Компонент поиска

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -2,13 +2,14 @@ import { SidebarProps } from './Sidebar.props';
 import styles from './Sidebar.module.css';
 import cn from 'classnames';
 import { Menu } from '../Menu/Menu';
+import { Search } from '../../components';
 import Logo from '../logo.svg';
 
 export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
 	return (
 		<div className={cn(className, styles.sidebar)} {...props}>
 			<Logo className={styles.logo} />
-			<div>Поиск</div>
+			<Search />
 			<Menu />
 		</div>
 	);
